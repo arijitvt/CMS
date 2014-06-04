52b490d5906172a3b5b536399324e5ac79d4d206
Arijit Chattopadhyay <arijitnit06@gmail.com>
2014-06-04 15:26:26 -0400

    Adding the new version of the approach.

---
+++ normal/Parser.cpp
@@ -0,0 +1,7 @@
+#include <Parser.h>
+
+Parser::Parser() {
+
+	cout<<"This is parser"<<endl;
+}
+
---
+++ normal/Parser.h
@@ -0,0 +1,18 @@
+#ifndef PARSER_HH
+#define PARSER_HH
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Parser {
+	public:
+		explicit Parser();
+
+	private:
+		string inputFileName;
+};
+
+
+#endif
